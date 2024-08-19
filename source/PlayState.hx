c1fd982fa389a007ef700a3cb7a5c70d497ce5b4
Bonny Wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-08-18 21:53:55 -0300

    Bro

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -874,7 +874,7 @@ class PlayState extends MusicBeatState
 
 		// "GLOBAL" SCRIPTS
 		#if LUA_ALLOWED
-		var daScripts:Array<String> = ['hud', 'Ending', 'camFollow', 'Zoom Girlfriend', 'script4']; // I don't think I need to explain this 
+		var daScripts:Array<String> = ['hud', 'Ending', 'camFollow', 'script3', 'script4']; // I don't think I need to explain this 
 		for (script in daScripts) {
 			var scriptPath:String = Paths.getPreloadPath('scripts/' + script + '.lua');
 			if (OpenFlAssets.exists(scriptPath))
@@ -1183,7 +1183,7 @@ class PlayState extends MusicBeatState
 
 		// SONG SPECIFIC SCRIPTS
 		#if LUA_ALLOWED
-		var daScripts:Array<String> = ['script', 'script1', 'script2', 'script3', 'script4']; // I don't think I need to explain this 
+		var daScripts:Array<String> = ['Zoom Girlfriend', 'script1', 'script2', 'script3', 'script4']; // I don't think I need to explain this 
 		for (script in daScripts) {
 		var scriptPath:String = Paths.getPreloadPath('data/' + Paths.formatToSongPath(SONG.song) + '/' + script + '.lua');
 			if (OpenFlAssets.exists(scriptPath))
